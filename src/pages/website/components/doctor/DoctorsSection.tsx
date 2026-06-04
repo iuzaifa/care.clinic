6cdeb82fdec3ad921f9cc2780e7abd359eaaaf8e
Abu Huzaifa <abuhuzaifaw7@gmail.com>
2026-06-04 19:20:46 +0530

    mobile res

--- src/pages/website/components/doctor/DoctorsSection.tsx
+++ src/pages/website/components/doctor/DoctorsSection.tsx
@@ -55,7 +55,7 @@ const DoctorsSection : React.FC = () => {
   return (
     <>
         <section className="bg-white py-28">
-           <div className="max-w-7xl mx-auto"> 
+           <div className="max-w-7xl mx-auto sm:px-5"> 
               <SectionHeader className="text-center max-w-xl mx-auto" title="Meet Our Doctors" description="Our administration and support staff all have exceptional people skills and trained to assist you with all medical enquiries." />
                 <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-10 md:mt-20 mt-10">
                    {doctors.map((data, index) => {
